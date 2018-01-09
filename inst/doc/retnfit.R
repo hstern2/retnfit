a8814fe4274ab5f9d0d57ef7cefcddb51d42d460
HARRY A STERN <hstern2@bhc0002.bluehive.circ.private>
2018-01-09 11:56:47 -0500

    examples do not read from separate csv file

--- inst/doc/retnfit.R
+++ inst/doc/retnfit.R
@@ -1,19 +1,36 @@
 ### R code from vignette source 'retnfit.Rnw'
 
 ###################################################
-### code chunk number 1: retnfit.Rnw:57-83
+### code chunk number 1: retnfit.Rnw:57-100
 ###################################################
 library('retnfit')
-results <- parallelFit(read.csv('sampledata.csv'),
-                        max_parents=4,
-                        n_cycles=100000,
+
+i_exp <- as.integer(c(0,0,0,0,0,0,0,0,0,1,1,1,
+                        1,1,1,1,1,1,1,1,1,1,1,
+                        1,1,1,1))
+i_node <- as.integer(c(0,0,0,1,1,1,2,2,2,0,0,0,
+                        1,1,1,2,2,2,0,0,0,1,1,1,
+                        2,2,2))
+outcome <- as.integer(c(-1,0,1,-1,0,1,-1,0,1,-1,0,1,
+                        -1,0,1,-1,0,1,-1,0,1,-1,0,1,
+                        -1,0,1))
+value <- c(2.0,1.0,0.0,2.0,1.0,0.0,2.0,1.0,0.0,
+            2.0,1.0,0.0,2.0,1.0,0.0,2.0,1.0,0.0,
+            2.0,1.0,0.0,2.0,1.0,0.0,2.0,1.0,0.0)
+is_perturbation <- as.integer(c(0,0,1,0,0,0,0,0,0,0,0,
+                                0,0,0,1,0,0,0,0,0,0,0,
+                                0,0,0,0,1))
+indata <- data.frame(i_exp,i_node,outcome,value,is_perturbation)
+results <- parallelFit(indata,
+                        max_parents=1,
+                        n_cycles=10000,
                         n_write=10,
                         T_lo=0.001,
                         T_hi=1.0,
                         target_score=0,
-                        n_proc=12,
-                        logfile='a.log',
-                        seed=525108)
+                        n_proc=3,
+                        logfile='test.log',
+                        seed=12345)
 
 lowest_temp_results <- results[[1]]
 
--- tests/test_smallmodel.R
+++ tests/test_smallmodel.R
@@ -2,11 +2,22 @@ library(testthat)
 library(retnfit) 
 
 smallmodel_score <- function() {
-    i_exp <- as.integer(c(0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1))
-    i_node <- as.integer(c(0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2,0,0,0,1,1,1,2,2,2))
-    outcome <- as.integer(c(-1,0,1,-1,0,1,-1,0,1,-1,0,1,-1,0,1,-1,0,1,-1,0,1,-1,0,1,-1,0,1))
-    value <- c(2.0,1.0,0.0,2.0,1.0,0.0,2.0,1.0,0.0,2.0,1.0,0.0,2.0,1.0,0.0,2.0,1.0,0.0,2.0,1.0,0.0,2.0,1.0,0.0,2.0,1.0,0.0)
-    is_perturbation <- as.integer(c(0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1))
+
+    i_exp <- as.integer(c(0,0,0,0,0,0,0,0,0,1,1,1,
+                            1,1,1,1,1,1,1,1,1,1,1,
+                            1,1,1,1))
+    i_node <- as.integer(c(0,0,0,1,1,1,2,2,2,0,0,0,
+                            1,1,1,2,2,2,0,0,0,1,1,1,
+                            2,2,2))
+    outcome <- as.integer(c(-1,0,1,-1,0,1,-1,0,1,-1,0,1,
+                            -1,0,1,-1,0,1,-1,0,1,-1,0,1,
+                            -1,0,1))
+    value <- c(2.0,1.0,0.0,2.0,1.0,0.0,2.0,1.0,0.0,
+                2.0,1.0,0.0,2.0,1.0,0.0,2.0,1.0,0.0,
+                2.0,1.0,0.0,2.0,1.0,0.0,2.0,1.0,0.0)
+    is_perturbation <- as.integer(c(0,0,1,0,0,0,0,0,0,0,0,
+                                    0,0,0,1,0,0,0,0,0,0,0,
+                                    0,0,0,0,1))
     indata <- data.frame(i_exp,i_node,outcome,value,is_perturbation)
     results <- parallelFit(indata,
                             max_parents=1,
@@ -18,8 +29,11 @@ smallmodel_score <- function() {
                             n_proc=3,
                             logfile='test.log',
                             seed=12345)
-    message(results)
-    results[[1]]$unnormalized_score
+    
+    lowest_temp_results <- results[[1]]
+
+    lowest_temp_results$unnormalized_score
+
 }
    
 test_that("smallmodel", {
